518f218f4b3faaaf5e2fe07e0eaba4d207a3c2bd
Xi Liu <xi.liu@u.nus.edu>
2023-06-21 23:11:31 +0800

    modified fft, among other things, pls read comments

--- LightEvoFuncLin.py
+++ LightEvoFuncLin.py
@@ -5,8 +5,8 @@ from scipy.fft import fft, ifft
 Alpha = 5
 U = 2
 Gamma = 5
-zTot = 200
-Q = 1000
+zTot = 100
+Q = 100*100
 dz = zTot / Q
 nStart = -500
 L = 2 * 500  # lattice length
@@ -41,9 +41,9 @@ def EIni(n):
     return d * np.sqrt(2 * Alpha * C0 / Gamma) * np.exp(1j * U / 2 * n * d) * sech(np.sqrt(Alpha) * n * d)
 
 
-Ei = []
-for i in nRange:
-    Ei.append(EIni(i))
+Ei = np.array([EIni(i) for i in nRange])
+# for i in nRange:
+#     Ei.append(EIni(i))
 
 
 def avgPos(EVec):
@@ -99,41 +99,45 @@ def height(EVec):
     return np.max(np.abs(EVec))
 
 
-def FNLStepA(Eq, dx):
+def FNLStepA(Eq, dz):
+    #dx changed to dz for clarity of meaning
     A0 = []
     for j in range(N):
-        A0.append(np.exp(1j * Gamma * abs(Eq[2 * j]) ** 2 * dx) * Eq[2 * j])
+        A0.append(np.exp(1j * Gamma * np.abs(Eq[2 * j]) ** 2 * dz) * Eq[2 * j])
+    #abs() changed to np.abs() for consistency
 
     return A0
 
 
-def FNLStepB(Eq, dx):
+def FNLStepB(Eq, dz):
+    # dx changed to dz for clarity of meaning
     B0 = []
     for j in range(N):
-        B0.append(np.exp(1j * Gamma * abs(Eq[2 * j + 1]) ** 2 * dx) * Eq[2 * j + 1])
-
+        B0.append(np.exp(1j * Gamma * np.abs(Eq[2 * j + 1]) ** 2 * dz) * Eq[2 * j + 1])
+    # abs() changed to np.abs() for consistency
     return B0
 
 
-def LNLStep(Eq1, Eq2, dx):
+def LNLStep(Eq1, Eq2, dz):
+    # dx changed to dz for clarity of meaning
     A1 = []
     B1 = []
     for j in range(N):
-        A1.append(np.exp(1j * Gamma * abs(Eq1[j]) ** 2 * dx) * Eq1[j])
-        B1.append(np.exp(1j * Gamma * abs(Eq2[j]) ** 2 * dx) * Eq2[j])
-
+        A1.append(np.exp(1j * Gamma * np.abs(Eq1[j]) ** 2 * dz) * Eq1[j])
+        B1.append(np.exp(1j * Gamma * np.abs(Eq2[j]) ** 2 * dz) * Eq2[j])
+    # abs() changed to np.abs() for consistency
     C = []
 
     for j in range(N):
         C.append(A1[j])
         C.append(B1[j])
 
-    return C
+    return np.array(C)
 
 
-def h(k, x):
-    h0 = np.array([[-beta0(x), -C1(x) * np.exp(1j * k) - C2(x)],
-                   [-C1(x) * np.exp(-1j * k) - C2(x), -beta1(x)]])
+def h(k, z):
+    h0 = np.array([[-beta0(z), -C1(z) * np.exp(1j * k) - C2(z)],
+                   [-C1(z) * np.exp(-1j * k) - C2(z), -beta1(z)]])
 
     return h0
 
@@ -141,40 +145,52 @@ def h(k, x):
 K = np.array(range(N)) * 2 * np.pi / N
 
 
-def LIStepw(A, B, t, deltat):
-    x = fft(A)
-    y = fft(B)
+def LIStepw(A, B, z, deltaz):
+    #t changed to z
+    #deltat changed to deltaz
+    # x = fft(A)
+    x=ifft(A,norm="ortho")
+    #x should be ifft of A by definition in notes and documentation of scipy.fft
+    #norm added for consistency with definition in notes
+    # y = fft(B)
+    y=ifft(B,norm="ortho")
+    #y should be ifft of B by definition
+    # norm added for consistency with definition in notes
     u = []
     v = []
     for j in range(N):
         a = np.array([x[j], y[j]])
-        b = np.matmul(linalg.expm(-1j * h(K[j], t) * deltat), a)
+        b = np.matmul(linalg.expm(-1j * h(K[j], z) * deltaz), a)
         u.append(b[0])
         v.append(b[1])
 
-    w = ifft(u)
-    return w
+    # w = ifft(u)
+    w=fft(u,norm="ortho")
+    # norm added for consistency with definition in notes
+    sigma=fft(v,norm="ortho")
+    return w,sigma
 
 
-def LIStepz(A, B, t, deltat):
-    x = fft(A)
-    y = fft(B)
-    u = []
-    v = []
-    for j in range(N):
-        a = np.array([x[j], y[j]])
-        b = np.matmul(linalg.expm(-1j * h(K[j], t) * deltat), a)
-        u.append(b[0])
-        v.append(b[1])
+# def LIStepz(A, B, t, deltat):
+#     x = fft(A)
+#     y = fft(B)
+#     u = []
+#     v = []
+#     for j in range(N):
+#         a = np.array([x[j], y[j]])
+#         b = np.matmul(linalg.expm(-1j * h(K[j], t) * deltat), a)
+#         u.append(b[0])
+#         v.append(b[1])
+#
+#     z = ifft(v)
+#     return z
 
-    z = ifft(v)
-    return z
 
-
-def OneStep(Eq, tCurr, dt):
-    EA = FNLStepA(Eq, dt / 2)
-    EB = FNLStepB(Eq, dt / 2)
-    EA1 = LIStepw(EA, EB, tCurr, dt)
-    EB1 = LIStepz(EA, EB, tCurr, dt)
-    EqPlus = LNLStep(EA1, EB1, dt / 2)
+def OneStep(Eq, zCurr, dz):
+    EA = FNLStepA(Eq, dz / 2)
+    EB = FNLStepB(Eq, dz / 2)
+    # EA = LIStepw(EA, EB, tCurr, dz)
+    # EB1 = LIStepz(EA, EB, tCurr, dz)
+    w,sigma=LIStepw(EA,EB,zCurr,dz)
+    EqPlus = LNLStep(w, sigma, dz / 2)
     return EqPlus
